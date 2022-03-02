3dd395dff839430e523bed0f217acf4034a5f699
brGuirra <br.guirra@gmail.com>
2022-03-01 21:52:14 -0300

    feat: fetch data from coin api

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -19,19 +19,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        currencyPicker.dataSource = self
         currencyPicker.delegate = self
+        currencyPicker.dataSource = self
+
     }
 }
 
 //MARK: - UIPickerViewDelegate
 
 extension ViewController: UIPickerViewDelegate {
-    
+
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return coinManager.currencyArray[row]
     }
-    
+
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         let selectedCurrency = coinManager.currencyArray[row]
         
@@ -42,11 +43,11 @@ extension ViewController: UIPickerViewDelegate {
 //MARK: - UIPickerViewDataSource
 
 extension ViewController: UIPickerViewDataSource {
-    
+
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
-    
+
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return coinManager.currencyArray.count
     }
--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -10,11 +10,46 @@ import Foundation
 
 struct CoinManager {
     
-    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    private var apiKey: String {
+        get {
+            guard let filePath = Bundle.main.path(forResource: "Coin-Info", ofType: "plist") else {
+                fatalError("Couldn't find file 'Coin-Info'.")
+            }
+            
+            let plist = NSDictionary(contentsOfFile: filePath)
+            
+            guard let value = plist?.object(forKey: "API_KEY") as? String else {
+                fatalError("Couldn't find 'API_KEY' in 'Coin-Info'.")
+            }
+            
+            return value
+        }
+    }
     
-    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    
+    let currencyArray = ["BRL","CAD","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
     func getCoinPrice(for currency: String) {
+        let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
+        
+        performRequest(with: urlString)
+    }
+    
+    func performRequest(with urlString: String) {
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, request, error) in
+                if error != nil {
+                    return
+                }
+                
+                if let data = data {
+                    print("Data: \(data)")
+                }
+            }
+            
+            task.resume()
+        }
     }
 }
